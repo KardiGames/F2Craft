ad62cb9575440e58d523c409313fab58882b11ff
AlexKardi2 <alexkardi2@gmail.com>
2026-03-15 14:42:30 +0300

    Activate Active Entities
    Base buildings and unit
    Fighter moving, attacking and destroying.

---
+++ Assets/Scripts/Abstract/ActiveEntity.cs
@@ -0,0 +1,56 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ActiveEntity : MonoBehaviour
+{
+    
+    [SerializeField] private int _playerNumber=0;
+    [SerializeField] private int _hp=0;
+    [SerializeField] private ActiveEntitiesManager _activeEntitiesManager; //TODO delete SerField
+
+    public int PlayerNumber => _playerNumber;
+    public int HP => _hp;
+
+    public void Init(int playerNumber, int hp, ActiveEntitiesManager activeEntitiesManager)
+    {
+        if (_playerNumber != 0 || hp != 0 || _activeEntitiesManager!=null)
+        {
+            print("Error. Re-initiation. Canceling");
+            return;
+        }
+
+        if (hp <= 0 || activeEntitiesManager == null)
+        {
+            print ("Error! Active Entity initialisation aborted");
+            return;
+        }
+        
+        _playerNumber = playerNumber;
+        _hp = hp;
+        _activeEntitiesManager = activeEntitiesManager;
+        _activeEntitiesManager.AddEntity(this);
+    }
+
+    public void GetDamage (int damage)
+    {
+        if (damage <= 0)
+            return;
+        _hp-= damage;
+        
+        if (_hp<0)
+        {
+            _activeEntitiesManager.RemoveEntity(this);
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        if (_activeEntitiesManager == null)
+        {
+            print("ERROR! Active Entity hasn't initiated. Destroying");
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Units/BattleUnit.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class BattleUnit : ActiveEntity
+{
+    [SerializeField] private ActiveEntity _target;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private int _damage;
+    [SerializeField] float _attackDistance;
+    [SerializeField] private float _attackCooldown;
+    private float _timeToAttack=0f;
+
+    private void Update()
+    {
+        if (_target == null)
+            return;
+
+        if (SqrDistanceTo(_target) > _attackDistance * _attackDistance)
+            Move();
+        else
+            Attack();
+    }
+
+    private float SqrDistanceTo (ActiveEntity target) 
+    {
+        if ( _target == null )
+        {
+            print("Error! No target for DistanceTo");
+            return float.MaxValue;
+        }
+        float sqrDistance = (transform.position - target.transform.position).sqrMagnitude; //TODO m.b. change to 2D distance &&|| cash transform
+        sqrDistance -= transform.localScale.x/2;
+        sqrDistance -= target.transform.localScale.x/2;
+        return sqrDistance;
+    }
+
+    private void Move ()
+    {
+        if (_target == null) 
+            return;
+
+        Vector3 moveVector = ((_target.transform.position - transform.position).normalized)*_moveSpeed*Time.deltaTime;
+        
+        transform.position = transform.position+ moveVector;
+
+    }
+
+    private void Attack()
+    {
+        if (_timeToAttack <= 0f)
+        {
+            _target.GetDamage(_damage);
+            _timeToAttack += _attackCooldown;
+        }
+        else
+            _timeToAttack-= Time.deltaTime;
+    }
+}
