7f9c536cbc3f218096daea082a96ec48b49708f7
AlexKardi2 <alexkardi2@gmail.com>
2026-04-24 08:20:26 +0300

    Building hotkeys
    - just 10

--- Assets/Scripts/UserInterface/HotkeyUserInterface.cs
+++ Assets/Scripts/UserInterface/HotkeyUserInterface.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 
 public class HotkeyUserInterface : MonoBehaviour
 {
-    private enum HotkeyState { Default, SelectBuilding, SelectItem, SelectUnit, SelectFoundation }
+    private enum HotkeyState { Default, SelectBuilding, SelectItemBlueprint, SelectUnit, SelectFoundation }
 
     [Header("General links")]
     [SerializeField] private TextUserInterface _textUI;
@@ -13,6 +14,11 @@ public class HotkeyUserInterface : MonoBehaviour
 
     [Header("Blueprints")]
     [SerializeField] private List<BlueprintForBuilding> _buildings = new List<BlueprintForBuilding>();
+    [SerializeField] private List<BlueprintForItem> _itemBlueprints = new List<BlueprintForItem>();
+    [SerializeField] private List<BlueprintForUnit> _unitBlueprints = new List<BlueprintForUnit>();
+
+    private List<string> _keyTexts = new List<string>() { "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P" };
+    private List<KeyCode> _keyCodes = new List<KeyCode>() { KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R, KeyCode.T, KeyCode.Y, KeyCode.U, KeyCode.I, KeyCode.O, KeyCode.P };
 
     private HotkeyState _hotkeyState = HotkeyState.Default;
     private BlueprintForBuilding _selectedBuilding;
@@ -25,12 +31,24 @@ public class HotkeyUserInterface : MonoBehaviour
     {
         if (_textUI == null || _camera == null || _foundationMask == 0)
             Debug.LogError("Link is not set");
+        if (_buildings.Count > _keyCodes.Count
+            || _itemBlueprints.Count > _keyCodes.Count
+            )
+            Debug.LogError("Crutch error. Some list is too long");
 
         _stateText.Add((int)HotkeyState.Default, "B - Build\n");
 
         _stateText.Add((int)HotkeyState.SelectBuilding, "Select building\n");
         for (int i = 0; i < _buildings.Count; i++)
-            _stateText[(int)HotkeyState.SelectBuilding] += i+" - "+_buildings[i].ConstructingBuilding.name + "\n";
+            _stateText[(int)HotkeyState.SelectBuilding] += _keyTexts[i] + " - " + _buildings[i].ConstructingBuilding.name + "\n";
+
+        _stateText.Add((int)HotkeyState.SelectItemBlueprint, "Select item blueprint\n");
+        for (int i = 0; i < _itemBlueprints.Count; i++)
+            _stateText[(int)HotkeyState.SelectItemBlueprint] += _keyTexts[i] + " - " + _itemBlueprints[i].name + "\n";
+
+        _stateText.Add((int)HotkeyState.SelectUnit, "Select produced unit\n");
+        for (int i = 0; i < _unitBlueprints.Count; i++)
+            _stateText[(int)HotkeyState.SelectUnit] += _keyTexts[i] + " - " + _unitBlueprints[i].name + "\n";
 
         ResetState();
     }
@@ -69,8 +87,48 @@ public class HotkeyUserInterface : MonoBehaviour
                     ResetState();
                 break;
             case HotkeyState.SelectBuilding:
-                if (Input.GetKeyDown(KeyCode.B))
-                    print("lala");
+                if (TryGetIndexOfPressedKey(out int buildingIndex))
+                {
+                    _selectedBuilding = _buildings[buildingIndex];
+                    switch (_selectedBuilding.ConstructingBuilding)
+                    {
+                        case Factory factory:
+                            _hotkeyState = HotkeyState.SelectItemBlueprint;
+                            _textUI.Show(_stateText[(int)HotkeyState.SelectItemBlueprint]);
+                            break;
+                        case UnitProducer pruducer:
+                            _hotkeyState = HotkeyState.SelectUnit;
+                            _textUI.Show(_stateText[(int)HotkeyState.SelectUnit]);
+                            break;
+                        default:
+                            Debug.LogError("Building type is not detected");
+                            ResetState();
+                            break;
+                    }
+
+                }
+                else if (Input.GetKeyDown(KeyCode.Escape))
+                    ResetState();
+                break;
+
+            case HotkeyState.SelectItemBlueprint:
+                if (TryGetIndexOfPressedKey(out int itemBlueprintIndex))
+                {
+                    _hotkeyState = HotkeyState.SelectFoundation;
+                    _selectedItem = _itemBlueprints[itemBlueprintIndex];
+                    _textUI.Show("Select place for\n" + _selectedBuilding + "\n with blueprint for\n" + _selectedItem.name);
+                }
+                else if (Input.GetKeyDown(KeyCode.Escape))
+                    ResetState();
+                break;
+
+            case HotkeyState.SelectUnit:
+                if (TryGetIndexOfPressedKey(out int unitIndex))
+                {
+                    _hotkeyState = HotkeyState.SelectFoundation;
+                    _selectedUnit = _unitBlueprints[unitIndex];
+                    _textUI.Show("Select place for\n" + _selectedBuilding + "\n with blueprint for\n" + _selectedUnit.ProducedUnit.name);
+                }
                 else if (Input.GetKeyDown(KeyCode.Escape))
                     ResetState();
                 break;
@@ -92,5 +150,17 @@ public class HotkeyUserInterface : MonoBehaviour
         _selectedUnit = null;
         _hotkeyState = HotkeyState.Default;
         _textUI.Show(_stateText[(int)HotkeyState.Default]);
+    }
+
+    private bool TryGetIndexOfPressedKey(out int index)
+    {
+        index = -1;
+        for (int i = 0; i < _keyCodes.Count; i++)
+            if (Input.GetKeyDown(_keyCodes[i]))
+            {
+                index = i;
+                return true;
+            }
+        return false;
     }
 }
